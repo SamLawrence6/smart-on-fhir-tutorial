3faa97722c3449d2f0f03e203e8bcb4b6f2cd8ce
SamLawrence6 <samuel.lawrence1@nhs.net>
2026-08-19 13:47:20 +0100

    Add temperature data handling to patient object

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -47,6 +47,7 @@
           var diastolicbp = getBloodPressureValue(byCodes('85354-9'),'8462-4');
           var hdl = byCodes('2085-9');
           var ldl = byCodes('2089-1');
+          var temp = byCodes('8310-5');
 
           var p = defaultPatient();
           p.birthdate = patient.birthDate;
@@ -65,7 +66,8 @@
 
           p.hdl = getQuantityValueAndUnit(hdl[0]);
           p.ldl = getQuantityValueAndUnit(ldl[0]);
-
+          p.temp = getQuantityValueAndUnit(temp[0]);
+          
           ret.resolve(p);
         });
       } else {
@@ -89,6 +91,7 @@
       diastolicbp: {value: ''},
       ldl: {value: ''},
       hdl: {value: ''},
+      temp: {value: ''}
     };
   }
 
@@ -132,6 +135,7 @@
     $('#diastolicbp').html(p.diastolicbp);
     $('#ldl').html(p.ldl);
     $('#hdl').html(p.hdl);
+    $('#temp').html(p.temp);
   };
 
 })(window);
